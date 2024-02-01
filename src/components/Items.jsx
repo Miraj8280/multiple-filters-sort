633568d0eb1b3b234dff0c2b402642141ef7017f
Miraj Asraf <mirajasraf786@gmail.com>
2024-02-01 10:36:05 +0530

    Items are added

---
+++ src/components/Items.jsx
@@ -0,0 +1,59 @@
+export const items = [
+    {
+      name: "Adidas",
+      category: "Shoes",
+    },
+    {
+      name: "Bata",
+      category: "Shoes",
+    },
+    {
+      name: "PUMA",
+      category: "Shoes",
+    },
+    {
+      name: "Reebok",
+      category: "Shoes",
+    },
+    {
+      name: "Rolex",
+      category: "Watches",
+    },
+    {
+      name: "Sonata",
+      category: "Watches",
+    },
+    {
+      name: "Boat",
+      category: "Watches"
+    },
+    {
+      name: "Calvin Klein",
+      category: "Jeans",
+    },
+    {
+      name: "Lee",
+      category: "Jeans",
+    },
+    {
+      name: "Pepe Jeans",
+      category: "Jeans",
+    },
+    {
+      name: "Lenskart",
+      category: "Eyeglasses",
+    },
+    {
+      name: "Titan",
+      category: "Eyeglasses",
+    },
+    {
+      name: "Oakley",
+      category: "Eyeglasses",
+    },
+    {
+      name: "Polaroid",
+      category: "Eyeglasses",
+    }
+  ];
+  
